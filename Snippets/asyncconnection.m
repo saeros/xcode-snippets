0205a4f5dc2c1a04b71c5f65ec5126a3aed690f8
Yonic Surny <saeros001@gmail.com>
2015-04-08 13:37:35 +0200

    Update asyncconnection.m

--- Snippets/asyncconnection.m
+++ Snippets/asyncconnection.m
@@ -6,39 +6,27 @@
 // Completion Scope: Function or Method
 
 dispatch_async(dispatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^{
-    
     NSURL *wsURL = <#url#>;
     NSMutableURLRequest *request = [[NSMutableURLRequest alloc] initWithURL:wsURL cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:10.0f];
     [request setHTTPMethod:@"GET"];
-    
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:[[NSOperationQueue alloc] init]
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-                               
-                               NSInteger statusCode = ((NSHTTPURLResponse *)response).statusCode;
-                               
-                               if ([data length] > 0 && error == nil)
-                                   {
-                                       // data downloaded
-                                       
-                                       // handle data (parsing)
-                                   }
-                               else if ([data length] == 0 && error == nil)
-                                   {
-                                       // empty reply (no error, just no data)
-                                       
-                                       error = [NSError errorWithDomain:<#domain#>
-                                                                   code:0
-                                                               userInfo:@{@"status": [NSString stringWithFormat:@"%ld", (long)statusCode], @"message": @"Data is empty"}];
-                                   }
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue new] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        NSInteger statusCode = ((NSHTTPURLResponse *)response).statusCode;
+        if ([data length] > 0 && error == nil) {
+            // data downloaded
+            // handle data (parsing)
+        }
+        else if ([data length] == 0 && error == nil) {
+            // empty reply (no error, just no data)
+            error = [NSError errorWithDomain:<#domain#>
+                                        code:0
+                                    userInfo:@{@"status": [NSString stringWithFormat:@"%ld", (long)statusCode], @"message": @"Data is empty"}];
+        }
                                    
-                                   if (error)
-                                   {
-                                       // error
-                                       
-                                       // handle error (e.g. failure(error);)
-                                   }
-                                   
-                               }];
+       if (error){
+           // error
+           // handle error (e.g. failure(error);)
+       }
+       
+   }];
         
-    });
+});
