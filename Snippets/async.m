0f25f32e5f40a7158e0e88a20e92891870f00c7c
Yonic Surny <saeros001@gmail.com>
2015-04-08 13:35:12 +0200

    Update async.m

--- Snippets/async.m
+++ Snippets/async.m
@@ -5,14 +5,9 @@
 // Language: Objective-C
 // Completion Scope: Function or Method
 
-dispatch_async(dispatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^(void) {
-
+dispatch_async(dispatch_get_global_queue(<#dispatch_queue_priority_t priority#>, <#unsigned long flags#>), ^(void) 
     <#code#>
-    
     dispatch_async(dispatch_get_main_queue(), ^(void) {
-
         <#code#>
-
     });
-
 });
