7ac29f909bae627b3c03354b60f6d7ed5f33a805
Yonic Surny <saeros001@gmail.com>
2015-04-09 14:20:11 +0200

    Update lifecycleapp.m

--- Snippets/lifecycleapp.m
+++ Snippets/lifecycleapp.m
@@ -5,20 +5,14 @@
 // Language: Objective-C
 // Completion Scope: Class implementation
 
-#pragma mark - Application lifecycle
-////////////////////////////////////////////////////////////////////////////////
-
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+#pragma mark - Lifecycle
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
    _window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
     // Override point for customization after application launch.
     self.window.rootViewController = ({
-    
-        UIViewController *vc = [[ViewController alloc] init];
-        
+        UIViewController *vc = [ViewController new];
         vc;
-        
     });
     
     [self.window makeKeyAndVisible];
@@ -26,8 +20,7 @@
     return YES;
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application
-{
+- (void)applicationWillResignActive:(UIApplication *)application {
     // Sent when the application is about to move from active to inactive 
     // state. This can occur for certain types of temporary interruptions 
     // (such as an incoming phone call or SMS message) or when the user quits 
@@ -37,8 +30,7 @@
     // game.
 }
 
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
+- (void)applicationDidEnterBackground:(UIApplication *)application {
     // Use this method to release shared resources, save user data, invalidate 
     // timers, and store enough application state information to restore your 
     // application to its current state in case it is terminated later. 
@@ -46,22 +38,19 @@
     // instead of applicationWillTerminate: when the user quits.
 }
 
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
+- (void)applicationWillEnterForeground:(UIApplication *)application {
     // Called as part of the transition from the background to the inactive 
     // state; here you can undo many of the changes made on entering the 
     // background.
 }
 
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
+- (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the 
     // application was inactive. If the application was previously in the 
     // background, optionally refresh the user interface.
 }
 
-- (void)applicationWillTerminate:(UIApplication *)application
-{
+- (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. 
     // Save data if appropriate. See also applicationDidEnterBackground:.
 }
