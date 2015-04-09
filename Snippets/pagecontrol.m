6380bdd2e885b2666621edef02f038977ccd14e2
Yonic Surny <saeros001@gmail.com>
2015-04-09 14:25:40 +0200

    Update pagecontrol.m

--- Snippets/pagecontrol.m
+++ Snippets/pagecontrol.m
@@ -6,35 +6,32 @@
 // Completion Scope: Class implementation
 
 #pragma mark - UIPageControl delegate
-////////////////////////////////////////////////////////////////////////////////
 
 static BOOL pageControlIsChangingPage = NO;
 
 // user tapped on the dots: change the scroll view position
-- (IBAction)changePage:(UIPageControl *)sender 
-{
+- (IBAction)changePage:(UIPageControl *)sender {
 	CGRect frame = <#scrollview#>.frame;
 	frame.origin.x = frame.size.width * sender.currentPage;
 	frame.origin.y = 0;
 	[<#scrollview#> scrollRectToVisible:frame animated:YES];
 	
 	// when the animated scrolling finishes, scrollViewDidEndDecelerating will turn this off
-    pageControlIsChangingPage = YES;
+    	pageControlIsChangingPage = YES;
 }
 
-- (void)scrollViewDidScroll:(UIScrollView *)scrollView
-{
-    if (pageControlIsChangingPage)
-        return;
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+	if (pageControlIsChangingPage) {
+		return;
+	}
 	
 	// we switch pages at 50% across
-    CGFloat pageWidth = scrollView.frame.size.width;
-    int page = floorf((scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1;
+	CGFloat pageWidth = scrollView.frame.size.width;
+	NSUInteger page = floorf((scrollView.contentOffset.x - pageWidth / 2.0f) / pageWidth) + 1;
 	
 	<#pagecontrol#>.currentPage = page;
 }
 
-- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView 
-{
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
 	pageControlIsChangingPage = NO;
 }
